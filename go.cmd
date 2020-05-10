d24f391fd309d164f4be1b41db459becd77c2b20
Jeff <jeff.wardlaw@gmail.com>
2020-05-10 06:21:42 -0400

    updated go.cmd

--- go.cmd
+++ go.cmd
@@ -1,18 +1,29 @@
 @echo off
-::go.cmd
+:: go.cmd
+:: modify labels, put this file rin your path
+:: run from run dialog (win+r)
+:: go <label>
+:: go term
+:: go aterm
+:: go gh
+
 
 set loc=%1
 
 2>NUL CALL :%loc%
-exit /b
+goto exit
+
+:gh
+start brave --new-window "https://github.com/smaug9?tab=repositories"
+goto exit
 
 :term
 start "" "C:\Program Files\ConEmu\ConEmu64.exe" /run {Shells::PowerShell}
-exit /b
+goto exit
 
 :aterm
 start "" "C:\Program Files\ConEmu\ConEmu64.exe" /run {Shells::PowerShell (Admin)}
-exit /b
+goto exit
 
-:blah
+:exit
 exit /b
